e6ec74daaeedd118c1a7e309a765733a529fada4
Duncan Giles <duncan3142@gmail.com>
2020-03-08 14:28:36 +0000

    chore: refactor to recursive solution

--- src/int-to-words.js
+++ src/int-to-words.js
@@ -1,97 +1,101 @@
 const { UNITS, TENS, SCALES } = require('./number-words');
 
+const CHUNK_LENGTH = 3;
+
+// Cut input into 3 digit chunks, corresponding to SCALES, from right to left
+// Left pad where necessary
+// This means we'll be processing chunks from least significant to most significant
+
+function cutChunk(remainder) {
+  return {
+    value: remainder.slice(-CHUNK_LENGTH).padStart(CHUNK_LENGTH, '0'),
+    remainder: remainder.substring(0, remainder.length - CHUNK_LENGTH),
+  };
+}
+
+const UNITS_INDEX = 2;
+const TENS_INDEX = 1;
+const HUNDREDS_INDEX = 0;
+
+function processChunk(chunk, chunkIndex, collector) {
+  if (chunkIndex === SCALES.length) {
+    // Insufficient scale words to describe input
+    const result = [];
+    result.push('Out of range');
+    return result;
+  }
+
+  const { value, remainder } = chunk;
+
+  // Only process chunks with non zero components
+  if (value !== '000') {
+    // Split chunk into digits
+    const digits = value.split('').map((char) => parseInt(char, 10));
+
+    // If tens integer is 1, i.e. 10, then add 10 to units integer
+    if (digits[TENS_INDEX] === 1) {
+      digits[UNITS_INDEX] += 10;
+    }
+
+    // Attempt word lookups
+    const unitWord = UNITS[digits[UNITS_INDEX]];
+    const tensWord = TENS[digits[TENS_INDEX]];
+    const hundredsWord = UNITS[digits[HUNDREDS_INDEX]];
+    const scaleWord = SCALES[chunkIndex];
+
+    // Push found words onto words array, in reverse order
+    if (scaleWord !== '') {
+      collector.push(scaleWord);
+    }
+
+    if (unitWord !== '') {
+      collector.push(unitWord);
+    }
+
+    if (tensWord !== '') {
+      collector.push(tensWord);
+    }
+
+    // Add 'and' string where we have a units or tens word, if...
+    if (unitWord !== '' || tensWord !== '') {
+      // chunk has a hundreds word, or more chunks to follow
+      if (hundredsWord !== '' || remainder.length > 0) {
+        collector.push('and');
+      }
+    }
+
+    if (hundredsWord !== '') {
+      collector.push(`${hundredsWord} hundred`);
+    }
+  }
+
+  return remainder.length > 0
+    ? processChunk(cutChunk(remainder), chunkIndex + 1, collector)
+    : collector;
+}
+
 function intToWords(input) {
   // Regex to remove leading zeros and exclude non numeric chars
   const VALID_PATTERN = /^(0*)([0-9]+)$/;
-  const match = input
-    .trim()
-    .match(VALID_PATTERN);
+  const match = input.trim().match(VALID_PATTERN);
 
-  // Check is a number
+  // Check is a number is valid format
   if (match === null) {
     return 'NaN';
   }
 
+  // Is valid number, discard leading zeros (if any)
   const significantDigits = match[2];
 
-  // Check for zero
+  // Check for zero value
   if (significantDigits === '0') {
     return 'zero';
   }
 
-  // Split input into 3 digit chunks, corresponding to SCALES, from right to left.
-  // Left pad where necessary.
-  // This means we'll be processing chunks from least significant to most significant.
+  // Wordify each scale chunk recursively
+  const words = processChunk(cutChunk(significantDigits), 0, []);
 
-  const CHUNK_LENGTH = 3;
-  let start = significantDigits.length;
-  const chunks = [];
-  while (start > 0) {
-    const end = start;
-    start = Math.max(0, start - CHUNK_LENGTH);
-    const rawChunk = significantDigits.slice(start, end);
-    chunks.push(rawChunk.padStart(CHUNK_LENGTH, '0'));
-  }
-
-  // Check if we have enough scale words to describe the input
-  const chunksLen = chunks.length;
-  if (chunksLen > SCALES.length) {
-    return 'Out of range';
-  }
-
-  // Wordify each integer in each chunk
-  const words = [];
-  chunks.forEach((chunk, chunkIndex) => {
-    const ALL_ZERO_PATTERN = /^0+$/;
-    // Only process chunks with non zero components
-    if (chunk.match(ALL_ZERO_PATTERN) === null) {
-      // Split chunk into digits
-      const digits = chunk
-        .split('')
-        .map((char) => parseInt(char, 10));
-
-      const UNITS_INDEX = 2;
-      const TENS_INDEX = 1;
-      const HUNDREDS_INDEX = 0;
-
-      // If tens integer is 1, i.e. 10, then add 10 to units integer
-      if (digits[TENS_INDEX] === 1) {
-        digits[UNITS_INDEX] += 10;
-      }
-
-      // Attempt word lookups
-      const unitWord = UNITS[digits[UNITS_INDEX]];
-      const tensWord = TENS[digits[TENS_INDEX]];
-      const hundredsWord = UNITS[digits[HUNDREDS_INDEX]];
-      const scaleWord = SCALES[chunkIndex];
-
-      // Push found words onto words array, in reverse order
-      if (scaleWord !== '') {
-        words.push(scaleWord);
-      }
-
-      if (unitWord !== '') {
-        words.push(unitWord);
-      }
-
-      if (tensWord !== '') {
-        words.push(tensWord);
-      }
-
-      // Add 'and' string where we have a units or tens word, if...
-      if (unitWord !== '' || tensWord !== '') {
-        // chunk has a hundreds word, or more chunks to follow
-        if (hundredsWord !== '' || chunkIndex < (chunksLen - 1)) {
-          words.push('and');
-        }
-      }
-
-      if (hundredsWord !== '') {
-        words.push(`${hundredsWord} hundred`);
-      }
-    }
-  });
-
+  // Return result, correcting order
   return words.reverse().join(' ');
 }
 
