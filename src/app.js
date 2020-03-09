eb5b60f31158284071c35b8dc9690d5ec1b2e8ee
Duncan Giles <duncan3142@gmail.com>
2020-03-09 08:08:30 +0000

    feat: create server

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+
+const router = require('./routes');
+
+const app = express();
+app.use(logger('dev'));
+
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, _next) => {
+  // Send error
+  res.status(err.status || 500);
+  res.send(`Ooops, an error occoured: ${err.message}`);
+});
+
+module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const createError = require('http-errors');
+
+const router = express.Router();
+const { intToWords, errorCases } = require('../int-to-words');
+
+/* GET words handler. */
+router.get('/:int', (req, res, next) => {
+  const { int } = req.params;
+  // Convert to words
+  const result = intToWords(int);
+  // Check for error cases
+  const errorValues = Object.values(errorCases);
+  if (errorValues.includes(result)) {
+    next(createError(422, result));
+    return;
+  }
+  // Send response
+  res.status(200);
+  res.send(result);
+});
+
+module.exports = router;
